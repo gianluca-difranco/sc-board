224933e3f1094931f4bddb7d373ab6b17807485d
Gianluca Di Franco <gianluk30gmail.com>
2026-05-17 21:03:02 +0200

    Aggiornamento entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 echo "Running Django migrations..."
-python manage.py migrate
+python manage.py migrate --noinput
 
 echo "Starting server with Gunicorn..."
 exec gunicorn --bind 0.0.0.0:8080 --workers 3 bacheca_project.wsgi:application
